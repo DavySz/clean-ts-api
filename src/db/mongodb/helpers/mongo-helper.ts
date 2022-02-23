4059c69330ca9daa4c7bc1778bb9a4a65fe2b2dd
Davy de Souza Assunção <davydesouzabar@gmail.com>
2022-02-23 16:31:44 -0400

    feat: connect to mongodb before app listen

--- src/db/mongodb/helpers/mongo-helper.ts
+++ src/db/mongodb/helpers/mongo-helper.ts
@@ -2,19 +2,19 @@ import { AccountModel } from './../../../domain/models/account'
 import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
-    client: null as MongoClient,
-    async connect (uri: string): Promise<void> {
-        this.client = await MongoClient.connect(process.env.MONGO_URL)
-    },
-    async disconnect (): Promise<void> {
-        await this.client.close()
-    },
-    getCollection (name: string): Collection {
-        return this.client.db().collection(name)
-    },
-    map (collection: any): any {
-		const { _id, ...collectionWithoutId } = collection
-		const account = Object.assign({}, collectionWithoutId, { id: _id.toHexString() }) as AccountModel
-		return account
-    }
+  client: null as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri)
+  },
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    const account = Object.assign({}, collectionWithoutId, { id: _id.toHexString() }) as AccountModel
+    return account
+  }
 }
