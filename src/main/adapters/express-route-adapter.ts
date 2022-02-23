96729785324a89f01f3eb9858c22286980696fac
Davy de Souza Assunção <davydesouzabar@gmail.com>
2022-02-23 19:18:07 -0400

    feat: add ExpressRouteAdapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,12 @@
+import { Controller, htppRequest } from './../../presentation/protocols'
+import { Request, Response } from 'express'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: htppRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
